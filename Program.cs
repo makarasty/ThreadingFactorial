4918348a959c1a246934e20dc8391f90879f7147
MaKarastY <71918286+makarasty@users.noreply.github.com>
2024-09-30 10:46:47 +0300

    i use Parallel.For mb i need to use async Task

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+	static void Main()
+	{
+		const int arraySize = 100;
+		int[] array = new int[arraySize];
+		System.Numerics.BigInteger[] factorials = new System.Numerics.BigInteger[arraySize];
+
+		for (int i = 0; i < arraySize; i++)
+		{
+			array[i] = i + 1;
+		}
+
+		// Можна було і await Task.WhenAll(...) але треба використовувати async Task...
+		Parallel.For(0, arraySize, i =>
+		{
+			factorials[i] = Factorial(array[i]);
+		});
+
+		for (int i = 0; i < arraySize; i++)
+		{
+			Console.WriteLine($"Факторіал числа: {array[i]}! = {factorials[i]}");
+		}
+	}
+
+	static System.Numerics.BigInteger Factorial(int n)
+	{
+		if (n == 0)
+		{
+			return 1;
+		}
+
+		return n * Factorial(n - 1);
+	}
+}
